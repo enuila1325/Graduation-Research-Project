8893e7378dfa65ec8cc14fdb19224d545ddf6672
enuila1325 <enuila634@gmail.com>
2023-12-04 11:13:32 -0600

    Adding prediagnosis classification to GUI

--- Prototipo/codes/scripts/main_gui_frame.py
+++ Prototipo/codes/scripts/main_gui_frame.py
@@ -3,12 +3,9 @@ import tkinter.ttk as ttk
 import torch
 import cv2
 from tkinter.filedialog import askopenfilename
-from image_classificator_nn import image_classificator as alzheimer_classificator
-from brain_tumor_classification_nn import (
-    image_classificator as brain_tumor_classificator,
-)
-from tuberculosis_classification_nn import (
-    image_classificator as tuberculosis_classificator,
+from image_classificator_nn import image_classificator
+from prediagnosis_classification_nn import (
+    image_classificator as prediagnosis_classifier,
 )
 
 from torchvision.transforms import transforms
@@ -35,7 +32,7 @@ def specific_prognosis_frame():
     alzheimer_detection = tk.Button(
         app,
         text="Detección y clasificación de Alzheimer",
-        command=lambda: open_file_choser(3),
+        command=lambda: open_file_choser(0),
     )
     brain_tumor_detection.place(x=220, y=50)
     tuberculosis_pneumonia_detection.place(x=200, y=100)
@@ -54,13 +51,12 @@ def open_file_choser(disease):
     )
     label.pack(padx=5)
     frame.pack(padx=10, pady=50, expand=True, fill=tk.BOTH)
-    predict_class = ""
-    if disease == 1:
-        predict_class = brain_tumor_detection(filename)
-    elif disease == 2:
-        predict_class = tuberculosis_detection(filename)
-    elif disease == 3:
-        predict_class = alzheimer_detection(filename)
+    disease_to_detect = ""
+    if disease == 4:
+        diagnosis_number = image_classification(filename)
+        predict_class = disease_prediction(filename, diagnosis_number)
+    else:
+        predict_class = disease_prediction(filename, disease)
     button = tk.Button(
         app_aux,
         text="ANALIZAR IMAGEN CARGADA",
@@ -69,6 +65,17 @@ def open_file_choser(disease):
     button.place(x=250, y=200)
     app_aux.mainloop()
     predict_class = ""
+
+
+def disease_prediction(filename, disease):
+    predict_class = ""
+    if disease == 1:
+        predict_class = brain_tumor_detection(filename)
+    elif disease == 2:
+        predict_class = tuberculosis_detection(filename)
+    elif disease == 0:
+        predict_class = alzheimer_detection(filename)
+    return predict_class
 
 
 def diagnosis_frame(predicted_class):
@@ -81,6 +88,33 @@ def diagnosis_frame(predicted_class):
     app.mainloop()
 
 
+def image_classification(image_route):
+    images_transform = transforms.Compose(
+        [
+            transforms.ToPILImage(),
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+        ]
+    )
+    model = torch.load(
+        "./codes/models/second-training-session/prediagnosis-model.pth",
+        map_location="cuda",
+    )
+    red = prediagnosis_classifier(3).to("cuda")
+    red.load_state_dict(model["model_state_dict"])
+    images = cv2.imread(image_route)
+    images = cv2.cvtColor(images, cv2.COLOR_BGR2RGB)
+    image_to_predict = images_transform(images)
+    image_to_predict = torch.unsqueeze(image_to_predict, 0)
+    red.eval()
+    with torch.no_grad():
+        output = red(image_to_predict.to("cuda"))
+    output_label = torch.topk(output, 1)
+    class_number = int(output_label.indices)
+    return class_number
+
+
 def alzheimer_detection(image_route):
     images_transform = transforms.Compose(
         [
@@ -91,10 +125,10 @@ def alzheimer_detection(image_route):
         ]
     )
     model = torch.load(
-        "Prototipo/codes/models/model_alzheimer-47_epochs-93Validation.pth",
+        "./codes/models/second-training-session/alzheimer-model.pth",
         map_location="cuda",
     )
-    red = alzheimer_classificator(4).to("cuda")
+    red = image_classificator(4).to("cuda")
     red.load_state_dict(model["model_state_dict"])
     classes = [
         "Mild Demented",
@@ -124,10 +158,10 @@ def brain_tumor_detection(image_route):
         ]
     )
     model = torch.load(
-        "Prototipo/codes/models/model_tumor.pth",
+        "./codes/models/second-training-session/tumor-model.pth",
         map_location="cuda",
     )
-    red = brain_tumor_classificator(4).to("cuda")
+    red = image_classificator(4).to("cuda")
     classes = ["glioma", "meningioma", "no_tumor", "pituitary"]
     images = cv2.imread(image_route)
     images = cv2.cvtColor(images, cv2.COLOR_BGR2RGB)
@@ -152,11 +186,12 @@ def tuberculosis_detection(image_route):
     )
     device = "cuda"
     model = torch.load(
-        "Prototipo/codes/models/model_tuberculosis.pth", map_location="cuda"
+        "./codes/models/second-training-session/tuberculosis-pneumonia-model.pth",
+        map_location="cuda",
     )
-    red = tuberculosis_classificator(2).to("cuda")
+    red = image_classificator(3).to("cuda")
     red.load_state_dict(model["model_state_dict"])
-    classes = ["healthy lungs", "tuberculosis"]
+    classes = ["healthy lungs", "pneumonia", "tuberculosis"]
     images = cv2.imread(image_route)
     images = cv2.cvtColor(images, cv2.COLOR_BGR2RGB)
     image_to_predict = images_transform(images)
@@ -173,7 +208,9 @@ def main():
     app = App()
     app.geometry("720x320")
     diverse_classification_button = tk.Button(
-        app, text="Clasificar y diagnosticar una imágen"
+        app,
+        text="Clasificar y diagnosticar una imágen",
+        command=lambda: open_file_choser(4),
     )
     specific_classification_button = tk.Button(
         app, text="Hacer un diagnóstico en especifico", command=specific_prognosis_frame
